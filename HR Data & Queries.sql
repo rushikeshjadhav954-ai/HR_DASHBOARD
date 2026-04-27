d8fc4fbc7cdca3b677424a7365c66a0b75e7f365
rushikeshjadhav954-ai <rushikeshjadhav954@gmail.com>
2026-04-27 21:02:14 +0530

    Add files via upload

---
+++ HR Data & Queries.sql
@@ -0,0 +1,111 @@
+Create database HR_Data;
+use hr_data;
+select count(*) from hr;
+select * from hr;
+alter table hr modify column emp_id varchar(20);
+desc hr;
+select birthdate from hr;
+set sql_safe_updates =0;
+UPDATE  hr 
+SET hire_date = CASE
+
+    -- Format: 06/04/91 (DD/MM/YY)
+    WHEN hire_date LIKE '%/%' AND LENGTH(hire_date) = 8
+        THEN DATE_FORMAT(STR_TO_DATE(hire_date, '%d/%m/%y'), '%Y-%m-%d')
+
+    -- Format: 6/29/1984 (MM/DD/YYYY)
+    WHEN hire_date LIKE '%/%' AND LENGTH(hire_date) >= 9
+        THEN DATE_FORMAT(STR_TO_DATE(hire_date, '%m/%d/%Y'), '%Y-%m-%d')
+
+    -- Format: 06-04-91 (DD-MM-YY)
+    WHEN hire_date LIKE '%-%' AND hire_date NOT LIKE '____-__-__'
+        THEN DATE_FORMAT(STR_TO_DATE(hire_date, '%d-%m-%y'), '%Y-%m-%d')
+
+    -- Format already correct: YYYY-MM-DD
+    WHEN hire_date LIKE '____-__-__'
+        THEN hire_date
+
+    ELSE NULL
+END;
+select hire_date from hr;
+UPDATE hr 
+SET termdate = DATE(STR_TO_DATE(termdate,'%Y-%m-%d %H:%i:%s UTC'))
+WHERE termdate IS NOT NULL 
+  AND termdate != '';
+  ALTER TABLE hr MODIFY COLUMN hire_date DATE;
+  UPDATE hr 
+SET termdate = DATE(STR_TO_DATE(termdate,'%Y-%m-%d %H:%i:%s UTC'))
+WHERE termdate IS NOT NULL 
+  AND termdate != '';
+UPDATE hr
+SET termdate = NULL
+WHERE termdate = '0000-00-00';
+alter table hr add column Age int;
+update hr set Age= timestampdiff(YEAR,birthdate,curdate());
+select birthdate,age from hr;
+select min(age),max(age) from hr;
+
+-- 1. What is the gender breakdown of employees in the company?
+select gender,count(*) from hr where age >=18 and  termdate is null group by gender;
+
+-- 2. What is the race/ethnicity breakdown of employees in the company?
+select race ,count(*) from hr where age >=18 and  termdate is null group by race order by count(*) desc;
+
+-- 3. What is the age distribution of employees in the company?
+select min(age),max(age) from hr where age >=18 and  termdate is null ;
+
+select case
+when age>=18 and age<= 24 then '18-24'
+when age>=25 and age<= 34 then '28-34'
+when age>=35 and age<= 44 then '35-44'
+when age>=45 and age<= 54 then '45-54'
+when age>=55 and age<= 64 then '55-64' 
+else '65+'
+end as age_group,gender,
+count(*) as count
+from hr
+where age >=18 and  termdate is null 
+group by age_group,gender
+order by age_group,gender;
+
+-- 4. How many employees work at headquarters versus remote locations?
+select location,count(*) as count from hr where  age >=18 and  termdate is null group by location; 
+
+-- 5. What is the average length of employment for employees who have been terminated?
+select round(avg(datediff(termdate,hire_date))/365),0 as avg_length_employment from hr 
+where termdate <= curdate() and termdate is not null and age>=18;
+
+-- 6. How does the gender distribution vary across departments and job titles?
+select department,gender,count(*) as count from hr 
+where age >=18 and  termdate is null 
+group by department,gender order by department;
+
+-- 7. What is the distribution of job titles across the company?
+select jobtitle,count(*) as count from hr 
+where age >=18 and  termdate is null 
+group by jobtitle
+order by jobtitle desc;
+
+-- 8. Which department has the highest turnover rate?
+select department,total_count,terminated_count,terminated_count/total_count as termination_rate from
+(select department,count(*) as total_count,
+sum(case when termdate is not null and termdate <= curdate() then 1 else 0 end) as terminated_count
+from hr
+where age>=18
+group by department) as subquery order by termination_rate desc;
+
+-- 9. What is the distribution of employees across locations by city and state?
+select location_state,count(*) as count from hr
+where age>=18 and termdate is null
+group by location_state order by count desc;
+
+-- 10. How has the company's employee count changed over time based on hire and term dates?
+select year,hires,terminations,hires-terminations as net_change,round((hires-terminations)/hires*100,2) as net_change_percent
+from(select year(hire_date) as year,count(*) as hires,
+sum(case when termdate is not null and termdate <= curdate() then 1 else 0 end) as terminations from hr where age>=18
+group by year(hire_date)) as subquery order by year asc;
+
+-- 11. What is the tenure distribution for each department?
+select department,round(avg(datediff(termdate,hire_date)/365),0) as avg_tenure from hr 
+where termdate<= curdate() and termdate is not null and age >= 18
+group by department;
